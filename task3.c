5e53432e7afa8303a436fd0dda08b777d20a6194
TheChyz <thechyz@hotmail.com>
2015-07-17 20:56:27 -0700

    Chyziak Update
    So I guess u just missed that for this version the threshold changes to 0x69000 haha. So it works. I also changed it to the new version of the function like in part 2 and 1, but I copied the rest from yours and it works (you missed some numbers that were suppose to be ints and kept as floats but thats just silly rushing mistake probably). 
    Not sure if there is some way we can optimize the int version that the float couldnt be optimized. So have a look and try and pry it apart, thats what I'm gonna do and if I get no idea tonight i'll just give up and say its good enough, cause damn its pretty good. (I want sub 1 sec tho :D).
    t = 1.15583483 and Program Size: Code=1412 RO-data=32 RW-data=14612 ZI-data=2284

--- task3.c
+++ task3.c
@@ -1,21 +1,46 @@
+
 #include "compact_coeff.h"
 #include "compact_R.h"
 
-
-const float DetThr=1e+15;
-float mod_out;
+const int DetThr=0x69000;
+int mod_out;
 int i,j,sample[256];
 
-int Timing_Synchronization(int *Samples, int *Coeff);
+float Timing_Synchronization(int *Samples, int *Coeff) {
+	int accReal = 0;
+	int accImaginary = 0;
+	int limit = 256;
+	int i;
+	if (Samples[129] == 0 && Samples[128] == 0) limit = 128;  //Can we assume 2 samples cannot be 0 right beside each other? ASK FABIO
+	for (i = 0; i < limit; i++) {
+		int samplesReal, coeffReal, samplesImaginary, coeffImaginary;
+		int sample = Samples[i];
+		if (sample != 0) {
+			
+			samplesReal = sample >> 16;   
+			samplesImaginary = (sample << 16) >> 16; 
+			coeffReal = Coeff[i] >> 16; 
+			coeffImaginary = (Coeff[i] << 16) >> 16;
+			//because (a+ib)*(c+id) = (ac - db) + (ad + bc)i
+			accReal += ((samplesReal*coeffReal) - (samplesImaginary*coeffImaginary)) >> 16;
+			accImaginary += ((samplesReal*coeffImaginary) + (samplesImaginary*coeffReal)) >> 16;
+		}	
+		sample = Samples[++i];
+		if (sample != 0) {
+			
+			samplesReal = sample >> 16;  
+			samplesImaginary = (sample << 16) >> 16; 
+			coeffReal = Coeff[i] >> 16;  
+			coeffImaginary = (Coeff[i] << 16) >> 16;
+			//because (a+ib)*(c+id) = (ac - db) + (ad + bc)i
+			accReal += ((samplesReal*coeffReal) - (samplesImaginary*coeffImaginary)) >> 16;
+			accImaginary += ((samplesReal*coeffImaginary) + (samplesImaginary*coeffReal)) >> 16;
+		}
+}
+	return (accReal*accReal) + (accImaginary*accImaginary);  //Real(acc)^2 + Imag(acc)^2		
+}
 
-/***
-problem, if Timing_Synchronization works on ints, mod_out is never larger than 2147483647
-which is way smaller than 1e+15 so the program never ends.  So either we need to reduce
-the detection threshold, or find another solution.
 
-Maybe unsigned? Ask Fabio.
-
-*/
 int main()
 {
 	for(j=0;j<256;j++) sample[j]=0; // intialization, not necessary on a simulator, may be needed on a board
@@ -34,40 +59,3 @@ int main()
 	}
 }
 
-int Timing_Synchronization(int *Samples, int *Coeff) { //t = 2.85090367
-	int accReal = 0;
-	int accImaginary = 0;
-	int modulo = 0;
-	int i;
-	int limit = 256;
-	if (Samples[129] == 0 && Samples[128] == 0) limit = 128;  //Can we assume 2 samples cannot be 0 right beside each other? ASK FABIO
-	for (i = 0; i < limit; i++) {
-		int sample = Samples[i];
-		if (sample != 0) {
-
-			int samplesReal = sample >> 16;
-			int coeffReal = Coeff[i] >> 16;
-			int samplesImaginary = (sample << 16) >> 16;
-			int coeffImaginary = (Coeff[i] << 16) >> 16;
-			//because (a+ib)*(c+id) = (ac - db) + (ad + bc)i
-			accReal += (samplesReal*coeffReal) - (samplesImaginary*coeffImaginary);
-			accImaginary += (samplesReal*coeffImaginary) + (samplesImaginary*coeffReal);
-		}
-		sample = Samples[++i];
-		if (sample != 0) {
-
-			int samplesReal = sample >> 16;
-			int coeffReal = Coeff[i] >> 16;
-			int samplesImaginary = (sample << 16) >> 16;
-			int coeffImaginary = (Coeff[i] << 16) >> 16;
-			//because (a+ib)*(c+id) = (ac - db) + (ad + bc)i
-			accReal += ((samplesReal*coeffReal) - (samplesImaginary*coeffImaginary)) >> 16;
-			accImaginary += ((samplesReal*coeffImaginary) + (samplesImaginary*coeffReal)) >> 16;
-		}
-	}
-	modulo = (accReal*accReal) + (accImaginary*accImaginary);  //Real(acc)^2 + Imag(acc)^2
-
-	return modulo; //now executes in 0.00011767 sec using int
-
-
-}
