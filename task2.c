dc75ce59d19b2fa487e69180372b7d5ebdbfca9b
TheChyz <thechyz@hotmail.com>
2015-07-20 21:07:21 -0700

    Updated task2
    Based on work done when we did task3. i++ instead of Sample[++i], etc

--- task2.c
+++ task2.c
@@ -9,25 +9,28 @@ float Timing_Synchronization(int *Samples, int *Coeff) {
 	int i;
 	if (Samples[129] == 0 && Samples[128] == 0) limit = 128;  //Can we assume 2 samples cannot be 0 right beside each other? ASK FABIO
 	for (i = 0; i < limit; i++) {
-		int samplesReal, coeffReal, samplesImaginary, coeffImaginary;
+		int samplesReal, coeffReal, samplesImaginary, coeffImaginary, coeff;
 		int sample = Samples[i];
 		if (sample != 0) {
 			
 			samplesReal = sample >> 16;   
 			samplesImaginary = (sample << 16) >> 16; 
-			coeffReal = Coeff[i] >> 16; 
-			coeffImaginary = (Coeff[i] << 16) >> 16;
+			coeff = Coeff[i];
+			coeffReal = coeff >> 16; 
+			coeffImaginary = (coeff << 16) >> 16;
 			//because (a+ib)*(c+id) = (ac - db) + (ad + bc)i
 			accReal += (samplesReal*coeffReal) - (samplesImaginary*coeffImaginary);
 			accImaginary += (samplesReal*coeffImaginary) + (samplesImaginary*coeffReal);
 		}	
-		sample = Samples[++i];
+		i++;
+		sample = Samples[i];
 		if (sample != 0) {
 			
 			samplesReal = sample >> 16;  
 			samplesImaginary = (sample << 16) >> 16; 
-			coeffReal = Coeff[i] >> 16;  
-			coeffImaginary = (Coeff[i] << 16) >> 16;
+			coeff = Coeff[i];
+			coeffReal = coeff >> 16; 
+			coeffImaginary = (coeff << 16) >> 16;
 			//because (a+ib)*(c+id) = (ac - db) + (ad + bc)i
 			accReal += (samplesReal*coeffReal) - (samplesImaginary*coeffImaginary);
 			accImaginary += (samplesReal*coeffImaginary) + (samplesImaginary*coeffReal);
@@ -59,3 +62,4 @@ int main()
 		};
 	}
 } 
+
