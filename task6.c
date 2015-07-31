4f9e158589ee18e947374b1b87408a59ad14f9fe
nzavagli <natezavaglia@gmail.com>
2015-07-31 14:40:32 -0700

    No changes
    Just added comments to task6.c no actual code changes.

--- task6.c
+++ task6.c
@@ -44,6 +44,7 @@ int Timing_Synchronization_int(int *Samples, int *Coeff) {
 	int accReal = 0;
 	int accImaginary = 0;
 	int i;
+	//Functionall the same as float version above.
 	for (i = 0; i < 256; i++) {
 		int samplesReal, coeffReal, samplesImaginary, coeffImaginary, coeff;
 		int sample = Samples[i];
@@ -53,6 +54,8 @@ int Timing_Synchronization_int(int *Samples, int *Coeff) {
 		coeffReal = coeff >> 16;
 		coeffImaginary = (coeff << 16) >> 16;
 		//because (a+ib)*(c+id) = (ac - db) + (ad + bc)i
+		//results of each multiplication shifted right 16 bits to prevent overflow errors
+		//As this causes truncation/reduction of the values, the detection threshold is adjusted accordingly
 		accReal += ((samplesReal*coeffReal)>>16) - ((samplesImaginary*coeffImaginary) >> 16);
 		accImaginary += ((samplesReal*coeffImaginary)>>16) + ((samplesImaginary*coeffReal) >> 16);
 
@@ -72,34 +75,36 @@ int Timing_Synchronization_int(int *Samples, int *Coeff) {
 
 
 int Timing_Synchronization_inline(int *Samples, int *Coeff) {
-	//hoping samples is in r0 and coeff is in r1 because r0-r3 reserved for functions
+	//Samples is in r0 and coeff is in r1 because r0-r3 reserved for functions
 	//accReal, accImaginary, i, samplesReal, samplesImaginary, coeffReal, coeffImaginary  are from 4-10 respectively. accDummy1/sample, accDummy2 are r2 and r3
 	//at the end when storing in dummy2, make it r0 (only at this time tho)
 	int modulo = 0;
-	asm("MOV r4, #0");
-	asm("MOV r5, #0");
-	asm("MOV r6, #0");
+	asm("MOV r4, #0"); //initializing accReal
+	asm("MOV r5, #0"); //initializing accImaginary
+	asm("MOV r6, #0"); //initializing loop counter
+
 	asm("firstLoop:");
-	asm("LDR r2, [r0, r6, LSL #2]");
-	asm("MOV r7, r2, ASR #16");
-	asm("MOV r8, r2, LSL #16");
-	asm("MOV r8, r8, ASR #16");
+	asm("LDR r2, [r0, r6, LSL #2]"); //
+	asm("MOV r7, r2, ASR #16"); //load samplesReal into R7
+	asm("MOV r8, r2, LSL #16"); //load samplesImaginary into R8
+	asm("MOV r8, r8, ASR #16"); //shift with sign samplesImaginary to get correct value
 	asm("LDR r2, [r1, r6, LSL #2]");
-	asm("MOV r9, r2, ASR #16");
-	asm("MOV r10, r2, LSL #16");
-	asm("MOV r10, r10, ASR #16");
-	asm("MUL r2, r7, r9");
-	asm("MUL r3, r8, r10");
-	asm("ADD r4, r4, r2, ASR #16");
-	asm("SUB r4, r4, r3, ASR #16");
-	asm("MUL r2, r7, r10");
-	asm("MUL r3, r8, r9");
+	asm("MOV r9, r2, ASR #16"); //load  coeffReal into R9
+	asm("MOV r10, r2, LSL #16");//load coefffImaginary R10
+	asm("MOV r10, r10, ASR #16");//Shift with sign coeffImaginary to get correct value
+	asm("MUL r2, r7, r9"); //samplesReal * coeffReal
+	asm("MUL r3, r8, r10");//samplesImaginar*coeffImaginary
+	asm("ADD r4, r4, r2, ASR #16"); //accumulate accReal
+	asm("SUB r4, r4, r3, ASR #16"); //subtract off the samplesImaginary * coeffImaginary
+	asm("MUL r2, r7, r10"); //samplesReal*coeffImaginary
+	asm("MUL r3, r8, r9"); //samplesImaginary*coeffReal
 	asm("ADD r5, r5, r2, ASR #16");
-	asm("ADD r5, r5, r3, ASR #16");
+	asm("ADD r5, r5, r3, ASR #16");//last two lines are accumulate accImaginary
 
 
 	//unrolled second time
-	asm("ADD r6, r6, #1");
+	//same as above, first line increments the counter
+	asm("ADD r6, r6, #1"); //increment loop counter
 	asm("LDR r2, [r0, r6, LSL #2]");
 	asm("MOV r7, r2, ASR #16");
 	asm("MOV r8, r2, LSL #16");
@@ -154,7 +159,7 @@ int main(void) {
 	*SWITCH_CONTROL = 0xff; //switch as input
 	*LED = 0x0;//turn all LEDs off
 	while(1) {
-		if (*BUTTON == 0x2) {
+		if (*BUTTON == 0x2) { //Following if block calculates the threshold automatically
 			float max_float = 0;
 			int max_int = 0;
 			*LED = 0x0;//turn all LEDs off
@@ -167,19 +172,20 @@ int main(void) {
 				mod_out_int = Timing_Synchronization_int(sample, coef);
 				if (mod_out_float > max_float)
 				{
-					max_float = mod_out_float;
+					max_float = mod_out_float; //calculates the maximum mod_out_float
 				}
 				if (mod_out_int > max_int)
 				{
-					max_int = mod_out_int;
+					max_int = mod_out_int; //calculates the maximum mod_out_int
 				}
 			}
-			DetThr_float = 0.9 * max_float;
-			DetThr_int = 0.9 * max_int;
-			*LED = 0x1;
+			DetThr_float = 0.9 * max_float; //sets float mode detection threshold
+			DetThr_int = 0.9 * max_int; //sets integer mode detection threshold
+			*LED = 0x1; // turns on first LED to indicate calculation is complete
+			//in real world, this would be a signal to the next peice of code which determines synchronization
 		}
 
-		else if (*BUTTON == 0x1) {
+		else if (*BUTTON == 0x1) { //This if block determines the synchronization, using floating point, or integer calculation
 			//float version
 			if (*SWITCH == 0x1) {
 				synch_location = 0;
@@ -198,8 +204,8 @@ int main(void) {
 						//TURN LED 8 for float version
 						*LED = 0x80;
 						if (synch_location != 20) {
-							synch[synch_location] = i;
-							synch_location++;
+							synch[synch_location] = i; //returns the number, i, of the calculation which resulted in synchronization
+							synch_location++;          //and stores it in an array (maximum of 20 synchronizations
 						}
 
 					};
@@ -224,7 +230,7 @@ int main(void) {
 						//TURN LED 4 for int version
 						*LED = 0x8;
 						if (synch_location != 20) {
-							synch[synch_location] = i;
+							synch[synch_location] = i; //see comment in float version
 							synch_location++;
 						}
 					};
@@ -232,7 +238,7 @@ int main(void) {
 			}
 
 			//inline version
-			else if (*SWITCH == 0x4) {
+			else if (*SWITCH == 0x4) {//this version uses assembly inlining to perform the integer synchronization calculation
 				synch_location = 0;
 				for (i=0;i<20;i++) {
 					synch[i] = 0;
@@ -249,7 +255,7 @@ int main(void) {
 						//TURN LED 2 for inline version
 						*LED = 0x2;
 						if (synch_location != 20) {
-							synch[synch_location] = i;
+							synch[synch_location] = i; //see comment in float version
 							synch_location++;
 						}
 					};
@@ -257,16 +263,16 @@ int main(void) {
 			}
 
 			//error version
-			else if (*SWITCH != 0x0) {
-				int flashing;
+			else if (*SWITCH != 0x0)  //this if block causes LED's to flash continuously if they are in an incorrect configuration
+				int flashing;         //such as more than one switch or an incorrect switch. Flashing stops when switches set correctly
 				int flash;
 				while (1) {
 					for (flashing = 0; flashing < 15; flashing++) {
 						for (flash = 0; flash < 200000; flash++) {
-							*LED = 0xff;
+							*LED = 0xff; //flash on
 						}
 						for (flash = 0; flash < 200000; flash++) {
-							*LED = 0x0;
+							*LED = 0x0; //flash off
 						}
 					}
 					if (*SWITCH == 0x0 || *SWITCH == 0x1 || *SWITCH == 0x2 || *SWITCH == 0x4) {
